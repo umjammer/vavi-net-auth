b536d85d7d645f071406ff542cb4f2b5631a86bb
umjammer <umjammer@gmail.com>
2020-04-29 21:54:08 +0900

    update SeleniumUtil

--- src/main/java/vavi/net/auth/oauth2/amazon/SeleniumAuthUI.java
+++ src/main/java/vavi/net/auth/oauth2/amazon/SeleniumAuthUI.java
@@ -11,23 +11,29 @@ import java.util.Deque;
 import java.util.logging.Level;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import vavi.net.auth.oauth2.AuthUI;
 import vavi.util.Debug;
+
+import static vavix.util.selenium.SeleniumUtil.findElement;
+
+import vavix.util.selenium.SeleniumUtil;
 
 
 /**
  * SeleniumAuthUI.
  *
  * TODO use SeleniumUtil
+ *
+ * properties
+ * <ul>
+ * <li> "webdriver.chrome.driver" default: $PWD + "/bin/chromedriver"</li>
+ * <li> "webdriver.chrome.verboseLogging" defalut: false</li>
+ * <li> "com.google.chrome.app" default: "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"</li>
+ * </ul>
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
  * @version 0.00 2019/06/27 umjammer initial version <br>
@@ -58,24 +64,12 @@ public class SeleniumAuthUI implements AuthUI<WebDriver> {
 
     /** Create a JFrame with a JButton and a JFXPanel containing the WebView. */
     private void openUI(String url) {
-        String pwd = System.getProperty("user.dir");
-        System.setProperty("webdriver.chrome.driver", pwd + "/bin/chromedriver");
-        System.setProperty("webdriver.chrome.verboseLogging", "false");
-
-        ChromeOptions chromeOptions = new ChromeOptions();
-        String app = System.getProperty("com.google.chrome.app", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
-        chromeOptions.setBinary(app);
-//        chromeOptions.addArguments("--window-size=240x320");
-
-        driver = new ChromeDriver(chromeOptions);
-        driver.manage().window().setSize(new Dimension(480, 640));
-
+        driver = SeleniumUtil.getDriver(480, 640);
         process(url);
     }
 
     /** Creates a WebView and fires up */
     private void process(String url) {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
         driver.get(url);
 
         Deque<String> tasks = new ArrayDeque<>();
@@ -84,24 +78,17 @@ public class SeleniumAuthUI implements AuthUI<WebDriver> {
         while (!login) {
             try {
                 sleep();
-                wait.until(d -> {
-                    if (d == null) {
-                        throw new IllegalStateException("browser maight be closed");
-                    }
-                    String r = ((JavascriptExecutor) d).executeScript("return document.readyState;").toString();
-//Debug.println(r);
-                    return "complete".equals(r);
-                });
+                SeleniumUtil.waitFor(driver, 10);
                 String location = driver.getCurrentUrl();
 //Debug.println("location: " + location);
                 if (location.indexOf("www.amazon.co.jp/ap/signin") > -1) {
                     try {
                         WebElement element = null;
 //Debug.println("element: name = " + element.getTagName() + ", class = " + element.getAttribute("class") + ", id = " + element.getAttribute("id") + ", type = " + element.getAttribute("type"));
-                        if (!tasks.contains("email") && (element = findElement(By.id("ap_email"))) != null) {
+                        if (!tasks.contains("email") && (element = findElement(driver, By.id("ap_email"))) != null) {
                             element.sendKeys(email);
                             tasks.add("email");
-                        } else if (!tasks.contains("password") && (element = findElement(By.id("ap_password"))) != null) {
+                        } else if (!tasks.contains("password") && (element = findElement(driver, By.id("ap_password"))) != null) {
                             if (password != null) {
                                 element.sendKeys(password);
                                 tasks.add("password");
@@ -109,7 +96,7 @@ public class SeleniumAuthUI implements AuthUI<WebDriver> {
 Debug.println("no password");
                                 continue;
                             }
-                        } else if (!tasks.contains("captcha") && (element = findElement(By.id("ap_password"))) != null) {
+                        } else if (!tasks.contains("captcha") && (element = findElement(driver, By.id("ap_password"))) != null) {
                             if (password != null) {
                                 element.sendKeys(password);
                                 tasks.add("captcha");
@@ -146,16 +133,6 @@ e.printStackTrace();
     /** */
     private static void sleep() {
         try { Thread.sleep(300); } catch (InterruptedException e) {}
-    }
-
-    /** */
-    private WebElement findElement(By by) {
-        try {
-            return driver.findElement(by);
-        } catch (org.openqa.selenium.NoSuchElementException e) {
-Debug.println("not found: " + by);
-            return null;
-        }
     }
 
     /* @see vavi.net.auth.oauth2.AuthUI#getResult() */
--- src/main/java/vavix/util/selenium/SeleniumUtil.java
+++ src/main/java/vavix/util/selenium/SeleniumUtil.java
@@ -6,10 +6,16 @@
 
 package vavix.util.selenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import vavi.util.Debug;
 
 
 /**
@@ -21,10 +27,70 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class SeleniumUtil {
 
     /** */
+    private static void setEnv() {
+        String pwd = System.getProperty("user.dir");
+        if (System.getProperty("webdriver.chrome.driver") == null) {
+            System.setProperty("webdriver.chrome.driver", pwd + "/bin/chromedriver");
+        }
+Debug.println("webdriver.chrome.driver: " + System.getProperty("webdriver.chrome.driver"));
+        if (System.getProperty("webdriver.chrome.verboseLogging") == null) {
+            System.setProperty("webdriver.chrome.verboseLogging", "false");
+        }
+Debug.println("webdriver.chrome.verboseLogging: " + System.getProperty("webdriver.chrome.verboseLogging"));
+
+    }
+
+    /** headless */
+    public static WebDriver getDriver() {
+        setEnv();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        String app = System.getProperty("com.google.chrome.app", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+Debug.println("com.google.chrome.app: " + System.getProperty("com.google.chrome.app"));
+        chromeOptions.setBinary(app);
+
+        chromeOptions.addArguments("--headless");
+        WebDriver driver = new ChromeDriver(chromeOptions);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
+
+        return driver;
+    }
+
+    /** windowed */
+    public static WebDriver getDriver(int width, int height) {
+        setEnv();
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        String app = System.getProperty("com.google.chrome.app", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+Debug.println("com.google.chrome.app: " + System.getProperty("com.google.chrome.app"));
+        chromeOptions.setBinary(app);
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.manage().window().setSize(new Dimension(width, height));
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> driver.quit()));
+
+        return driver;
+    }
+
+    /** */
     public static void waitFor(WebDriver driver) {
         new WebDriverWait(driver, 10).until(
             d -> ((JavascriptExecutor) d).executeScript("return document.readyState").equals("complete"));
         try { Thread.sleep(300); } catch (InterruptedException e) {}
+    }
+
+    /** */
+    public static void waitFor(WebDriver driver, int delay) {
+        new WebDriverWait(driver, delay).until(d -> {
+            if (d == null) {
+                throw new IllegalStateException("browser maight be closed");
+            }
+            String r = ((JavascriptExecutor) d).executeScript("return document.readyState;").toString();
+//Debug.println(r);
+            return "complete".equals(r);
+        });
     }
 
     /** */
@@ -42,6 +108,16 @@ public class SeleniumUtil {
         c = 0;
         driver.getWindowHandles().forEach(h -> { System.err.println(c++ + ": " + h); });
         System.err.println("----------------------------");
+    }
+
+    /** */
+    public static WebElement findElement(WebDriver driver, By by) {
+        try {
+            return driver.findElement(by);
+        } catch (org.openqa.selenium.NoSuchElementException e) {
+Debug.println("not found: " + by);
+            return null;
+        }
     }
 }
 
