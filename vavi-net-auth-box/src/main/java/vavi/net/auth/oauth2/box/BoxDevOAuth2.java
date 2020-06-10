3f7ae8f81043133cbd758be7019a263a095fcc05
umjammer <umjammer@gmail.com>
2020-06-10 17:22:37 +0900

    add another oauth2 using dev token for box

---
+++ vavi-net-auth-box/src/main/java/vavi/net/auth/oauth2/box/BoxDevOAuth2.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2016 by Naohide Sano, All rights reserved.
+ *
+ * Programmed by Naohide Sano
+ */
+
+package vavi.net.auth.oauth2.box;
+
+import java.io.IOException;
+
+import com.box.sdk.BoxAPIConnection;
+
+import vavi.net.auth.UserCredential;
+import vavi.net.auth.oauth2.OAuth2;
+import vavi.net.auth.oauth2.OAuth2AppCredential;
+
+
+/**
+ * BoxDevOAuth2.
+ * <p>
+ * environment variable
+ * <li> BOX_DEVELOPER_TOKEN
+ *
+ * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
+ * @version 0.00 2016/02/29 umjammer initial version <br>
+ * @see "https://app.box.com/developers/console/app/216798/configuration"
+ */
+public class BoxDevOAuth2 implements OAuth2<UserCredential, BoxAPIConnection> {
+
+    /** */
+    public BoxDevOAuth2(OAuth2AppCredential appCredential) {
+    }
+
+    @Override
+    public BoxAPIConnection authorize(UserCredential userCredential) throws IOException {
+        return new BoxAPIConnection(System.getenv("BOX_DEVELOPER_TOKEN"));
+    }
+}
+
+/* */
