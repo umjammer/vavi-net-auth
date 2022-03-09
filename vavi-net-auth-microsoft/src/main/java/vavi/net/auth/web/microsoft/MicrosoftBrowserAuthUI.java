fe7742b22fc7aff382f39802b1f01e1a142c5531
Naohide Sano <namachapanda2005@gmail.com>
2022-03-09 17:21:00 +0900

    [microsoft] make it more durable

--- vavi-net-auth-microsoft/src/main/java/vavi/net/auth/web/microsoft/MicrosoftBrowserAuthUI.java
+++ vavi-net-auth-microsoft/src/main/java/vavi/net/auth/web/microsoft/MicrosoftBrowserAuthUI.java
@@ -19,15 +19,12 @@ import vavi.net.auth.oauth2.OAuth2AppCredential;
 import vavi.net.http.HttpServer;
 import vavi.util.Debug;
 
-import vavix.util.selenium.SeleniumUtil;
-
 
 /**
  * MicrosoftBrowserAuthUI.
  *
  * @author <a href="mailto:umjammer@gmail.com">Naohide Sano</a> (umjammer)
  * @version 0.00 2021/10/29 umjammer initial version <br>
- * @see SeleniumUtil
  */
 public class MicrosoftBrowserAuthUI implements AuthUI<String> {
 
@@ -56,44 +53,44 @@ Debug.println("totpSecret: " + totpSecret);
 
     @Override
     public void auth() {
-    	try {
-        	cdl = new CountDownLatch(1);
+        try {
+            cdl = new CountDownLatch(1);
 
-	        URL redirectUrl = new URL(this.redirectUrl);
-	        String host = redirectUrl.getHost();
-	        int port = redirectUrl.getPort();
+            URL redirectUrl = new URL(this.redirectUrl);
+            String host = redirectUrl.getHost();
+            int port = redirectUrl.getPort();
 
-	        httpServer = new HttpServer(host, port);
-	        httpServer.addRequestListener((req, res) -> {
-	        	String location = req.getRequestURI();
+            httpServer = new HttpServer(host, port);
+            httpServer.addRequestListener((req, res) -> {
+                String location = req.getRequestURI();
 Debug.println("uri: " + location);
-	            res.setContentType("plain/text");
-	            PrintWriter os = res.getWriter();
-	            os.println("code: " + location.substring(location.indexOf("code=") + "code=".length(), location.indexOf("&")));
-	            os.flush();
-	            code = this.redirectUrl + location;
-	            cdl.countDown();
-	        });
-	        httpServer.start();
+                res.setContentType("plain/text");
+                PrintWriter os = res.getWriter();
+                os.println("code: " + location.substring(location.indexOf("code=") + "code=".length(), location.lastIndexOf("&") > 0 ? location.lastIndexOf("&") : location.length()));
+                os.flush();
+                code = this.redirectUrl + location;
+                cdl.countDown();
+            });
+            httpServer.start();
 
-			Desktop.getDesktop().browse(URI.create(url));
-		} catch (IOException e) {
-			exception = e;
-		}
+            Desktop.getDesktop().browse(URI.create(url));
+        } catch (IOException e) {
+            exception = e;
+        }
     }
 
     @Override
     public String getResult() {
         try {
-        	cdl.await();
-			httpServer.stop();
-		} catch (IOException | InterruptedException e) {
-			if (exception == null) {
-				exception = e;
-			} else {
-				exception.addSuppressed(e);
-			}
-		}
+            cdl.await();
+            httpServer.stop();
+        } catch (IOException | InterruptedException e) {
+            if (exception == null) {
+                exception = e;
+            } else {
+                exception.addSuppressed(e);
+            }
+        }
 Debug.println("return: " + code);
 
         return code;
@@ -106,6 +103,11 @@ Debug.println("return: " + code);
 
     @Override
     protected void finalize() {
+        try {
+            httpServer.stop();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
